a3fa6818d3e53d8ccdb3666ee8c32c9827d271c2
n-anselm <n-anselm@protonmail.com>
2025-06-06 19:50:05 +0100

    Added GH Pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,20 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
+const SERVER_PORT = 4321;
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+const LIVE_URL = "https://sternbecker.github.io"; // URL to access your site after deploying it
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCALHOST_URL;
+
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
+
 export default defineConfig({
+  server: { port: SERVER_PORT},
+  site: BASE_URL,
   vite: {
     plugins: [tailwindcss()]
   }
